6f0e00b9d833f9c7bed9345209166f30c2669748
Artur Big <bigartur41@gmail.com>
2024-10-16 12:10:50 +0300

    Revision #1

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ public class Main {
         long amount = 1_000_000; // Срок кредита в руб.
         double percent = 9.99;  // Процет годовой %
         int loanTern = 1;   // Срок кредита в годах
-        int result = service.calculate(amount, percent,loanTern);
+        int result = service.calculate(amount, percent, loanTern);
 
         System.out.println("Сумма кредита " + amount + " руб.");
         System.out.println("Срок кредита " + loanTern + " год.");
         System.out.println("Процентная ставка " + percent + " %");
         System.out.println("Ежемесячный платёж " + result + " руб.");
 
-      }
+    }
 }
